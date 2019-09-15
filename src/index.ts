00c9023a01d597b1c28481a13106c5415cb18b04
kmelct <kmelct@gmail.com>
2019-09-15 17:57:22 +0300

    fulcrum-logo: Impliment fulcrum logo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
+import chalk from 'chalk';
 import * as figlet from 'figlet';
-import * as utils from 'util';
-
-const figletAsync = utils.promisify(figlet);
 
 async function helloFulcrum() {
-  const logo = await figletAsync('Fulcrum Rocks');
-  console.log(logo);
+  const logo = figlet.textSync('FR', { font: '3D-ASCII' });
+  console.log(chalk.blueBright(logo));
 }
 
 helloFulcrum();
